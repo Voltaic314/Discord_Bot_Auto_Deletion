6a9eb167a9d067cf126b48f75e7b1d0688b04985
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2023-07-31 21:21:47 -0400

    creating the discord bot
    Note that this does not account for threads created (I don't think).

---
+++ discord_bot.py
@@ -0,0 +1,55 @@
+"""
+Author: Logan Maupin
+
+This is just a simple discord bot that deletes messages from a specified channel given certain parameters. Such as:
+What channel to delete from and how long should the messages last.
+"""
+import discord
+import asyncio
+import time
+
+
+class Focus_Bot_Client(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True  # Enable the GUILD_MEMBERS intent
+        super().__init__(intents=intents)
+        self.synced = False  # we use this so the bot doesn't sync commands more than once
+
+    async def on_ready(self):
+        # wait for the bot to be set up properly
+        await self.wait_until_ready()
+        if not self.synced:  # check if slash commands have been synced
+            await tree.sync()
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
+
+        # when we start up the bot, run the check to remove anyone in the database who shouldn't be in there anymore.
+        self.loop.create_task(self.bot_routines())
+
+    async def bot_routines(self):
+        await self.wait_until_ready()
+
+        server_id = "Your_Server_ID_Here"
+        guild = client.get_guild(server_id)
+        auto_delete_channel = guild.get_channel("Your_Channel_ID_Here")
+
+        # now we'll look to see if any messages need to be deleted from the auto delete channel
+        # if so, then delete them, if not just ignore.
+        while True:
+            async for message in auto_delete_channel.history(limit=None, oldest_first=True):
+
+                time_difference_of_current_vs_input = message.created_at.timestamp() - time.time()
+                one_day_in_epoch_seconds = 86400
+                message_is_overdue_for_deletion = time_difference_of_current_vs_input >= one_day_in_epoch_seconds
+
+                if not message.pinned and message_is_overdue_for_deletion:
+                    await message.delete()
+
+            await asyncio.sleep(60)
+
+
+client = Focus_Bot_Client()
+tree = discord.app_commands.CommandTree(client)
+TOKEN = "Your_discord_bot_api_key_here"
+client.run(TOKEN)
