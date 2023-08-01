e4905cb93ffb7549a514e4e390fe327a4008cbf2
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2023-07-31 21:40:46 -0400

    updating stuff

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 secret_stuff = {
     "server_id": 223662908582658048,
     "auto_delete_channel_id": 1084927876840833275,
-    "discord_bot_api_token": "MTEzNTc0NjY0NTk0Mjg3MDA4Nw.GBAhG1.e9Q_JAIXWmiH-BZB-vwZk2IK6roo83aNo6iLRQ",
+    "discord_bot_api_token": "MTEzNTc0NjY0NTk0Mjg3MDA4Nw.GRtamh.DrzCDN9D37uCGWWTGQ0hSs1UGfclds033zF0js",
     "application_id": 1135746645942870087,
 }
